012037de5be8fa0ed29af73437615794d3b34ce1
mattstermh93 <mattstermh@hotmail.com>
2018-06-28 14:27:03 -0400

    all pages functional, footer only working on index

--- app/routes.py
+++ app/routes.py
@@ -9,3 +9,19 @@ def index():
 @app.route('/who', methods=['GET', 'PUT'])
 def who():
     return render_template("who.html", title='Who We Are')
+
+@app.route('/what', methods=['GET', 'PUT'])
+def what():
+    return render_template("what.html", title='What We Do')
+
+@app.route('/news', methods=['GET', 'PUT'])
+def news():
+    return render_template("news.html", title='News and Events')
+
+@app.route('/where', methods=['GET', 'PUT'])
+def where():
+    return render_template("where.html", title='Where We Work')
+
+@app.route('/contact', methods=['GET', 'PUT'])
+def contact():
+    return render_template("contact.html", title='Contact Us')
