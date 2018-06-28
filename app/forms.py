64e4599146b544e03bce660c4f7bb9a035599534
mattstermh93 <mattstermh@hotmail.com>
2018-06-28 10:42:15 -0400

    header visible, no errors

---
+++ app/forms.py
@@ -0,0 +1,28 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, EqualTo, Email, Length, ValidationError
+from app.models import User
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Submit')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[Length(min=15, max=24), DataRequired()])
+    email = StringField('Email Address', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password', message='Passwords must match')])
+    submit = SubmitField('Submit')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Username already taken.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Email already taken.')
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,18 @@
-from app import app
-from flask import render_template, redirect, url_for
+from app import app, db
+from flask import render_template, redirect, url_for, flash, request
+from app.forms import LoginForm, RegistrationForm
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index', methods=['GET', 'PUT'])
 def index():
     return render_template("index.html", title='Home Page')
+
+@app.route('/redirect')
+def goaway():
+    return redirect(url_for('index'))
 
 @app.route('/who', methods=['GET', 'PUT'])
 def who():
@@ -25,3 +33,44 @@ def where():
 @app.route('/contact', methods=['GET', 'PUT'])
 def contact():
     return render_template("contact.html", title='Contact Us')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    register_form = RegistrationForm()
+    if register_form.validate_on_submit():
+        user = User(username=register_form.username.data, email=register_form.email.data)
+        user.set_password(register_form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now registered!')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=register_form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(username=login_form.username.data).first()
+        if user is None or not user.check_password(login_form.password.data):
+            flash('Invalid login credentials')
+            return redirect(url_for('login'))
+        login_user(user, remember=login_form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        flash('Thanks for logging in {}!'.format(current_user.username))
+        return redirect(next_page)
+    return render_template('login.html', form=login_form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/profile', methods=['GET', 'PUT'])
+def profile():
+    return render_template("profile.html", title='Contact Us')
