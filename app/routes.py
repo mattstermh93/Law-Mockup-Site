39fba628dae19a36c3c412aa7b4e69d8b9e0de92
mattstermh93 <mattstermh@hotmail.com>
2018-06-28 13:52:19 -0400

    added content to who

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,7 @@ from flask import render_template, redirect, url_for
 @app.route('/index', methods=['GET', 'PUT'])
 def index():
     return render_template("index.html", title='Home Page')
+
+@app.route('/who', methods=['GET', 'PUT'])
+def who():
+    return render_template("who.html", title='Who We Are')
