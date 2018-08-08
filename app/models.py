af5a3abe609232e7aa0aa14b3bd1922c80115dfe
mattstermh93 <mattstermh@hotmail.com>
2018-08-08 13:12:42 -0400

    adjusted models, added username

--- app/models.py
+++ app/models.py
@@ -21,12 +21,20 @@ class Role(db.Model):
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(255), unique=True)
     email = db.Column(db.String(255), unique=True)
     password_hash = db.Column(db.String(255))
     first_name = db.Column(db.String(50))
     last_name = db.Column(db.String(50))
     roles = db.relationship("UserRoles",
                     backref=db.backref("user", lazy="joined"))
+
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 @login.user_loader
 def load_user(id):
